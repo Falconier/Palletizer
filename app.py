c0fef8eb61897c9a188ae15713a53727dd80f74a
Jacob Emory Bullin <jacobbullin@roadrunner.com>
2025-04-30 19:29:56 -0400

    fushing things out - vibecoded the html and css - surprised at how well good it looks, I will have to tweak it and make it even better though

--- app.py
+++ app.py
@@ -22,7 +22,34 @@ def get_db_connection():
         print(f"Connection failed: {e}")
         raise
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
+@app.route('/add_inventory', methods=['POST'])
+def add_inventory():
+    data = request.get_json()
+    item_upc = data.get('item_upc')
+    item_mn = data.get('item_model_number')
+    item_pn = data.get('item_part_number')
+    item_name = data.get('item_name')
+    item_desc = data.get('item_description')
+    item_price = data.get('item_price')
+    item_cost = data.get('item_cost')
+    items_per_pallet = data.get('items_per_pallet')
+    in_box = data.get('in_box')
+    if in_box:
+        items_per_box = data.get('items_per_box')
+    
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('Insert into inventory (item_upc, item_model_number, item_part_number, item_name, item_description, item_price, item_cost, items_per_pallet, in_box, items_per_box) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                   (item_upc, item_mn, item_pn, item_name, item_desc, item_price, item_cost, items_per_pallet, in_box, items_per_box if in_box else None))               
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    return jsonify({"message": "Item added successfully", "item_id": item_id}), 201
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)

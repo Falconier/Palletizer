7ab0bdcf825b9597ac049e155c16af9b4a3b9e2c
Jacob Emory Bullin <jacobbullin@roadrunner.com>
2025-04-30 11:33:25 -0400

    setting up inital app file

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, request, render_template
+import pyodbc
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
+
+conn_str = (f"DRIVER={os.getenv('DB_DRIVER')};"
+            f"SERVER={os.getenv('DB_SERVER')};"
+            f"DATABASE={os.getenv('DB_NAME')};"
+            f"UID={os.getenv('DB_USER')};"
+            f"PWD={os.getenv('DB_PASSWORD')};")
+
+def get_db_connection():
+    try:
+        conn = pyodbc.connect(conn_str)
+        return conn
+    except pyodbc.Error as e:
+        print(f"Connection failed: {e}")
+        raise
+

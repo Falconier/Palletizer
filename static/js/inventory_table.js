87f788d99a6e7c7d982ecc2f5b96cfb03fa6de72
Jacob Emory Bullin <jacobbullin@roadrunner.com>
2025-05-02 13:22:07 -0400

    cleaning up the datatable information

--- static/js/inventory_table.js
+++ static/js/inventory_table.js
@@ -10,6 +10,8 @@ function loadInventory() {
             tbody.innerHTML = '';
             items.forEach(item => {
                 const row = document.createElement('tr');
+                let ipb = item.items_per_box ? item.items_per_box : 'N/A';
+                let ib = item.in_box ? 'Y' : 'N';
                 row.innerHTML = `
                     <td class="libre-barcode-128-text-regular">${item.item_upc}</td>
                     <td>${item.item_model_number}</td>
@@ -18,8 +20,8 @@ function loadInventory() {
                     <td>${item.item_description}</td>
                     <td>${item.item_price}</td>
                     <td>${item.items_per_pallet}</td>
-                    <td>${item.in_box}</td>
-                    <td>${item.items_per_box}</td>
+                    <td>${ib}</td>
+                    <td>${ipb}</td>
                     <td><button class="btn btn-info btn-sm" onclick="Details(${item.item_id})">View Details</button></td>
                 `;
                 tbody.appendChild(row);
